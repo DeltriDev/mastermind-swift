14658d11f9e166e723ae9f0baf0fdf3f21588b01
SamAlexandros <98485800+DeltriDev@users.noreply.github.com>
2024-07-06 11:27:09 +0300

    Full game working!

--- Sources/main.swift
+++ Sources/main.swift
@@ -6,6 +6,7 @@ print("You have 12 tries")
 //necessary variables
 var tries = 0
 let possibleCharacters:[String] = ["1","2","3","4","5","6","7","8"]
+let possibleCharactersString = "12345678"
 var correctAnswer:String = ""
 //generate the correct answer
 repeat
@@ -42,10 +43,25 @@ func AwaitAnswer() -> Bool
     {
         //FIXME: if you insert a 4-letter sequence of letters it doesnt stop, wooooops
         //FIXME: don't let the player type a character more than once
-        if unwrappedAnswer.count == 4 && !unwrappedAnswer.contains("9") && !unwrappedAnswer.contains("0") 
+        let arrayAnswer =  Array(unwrappedAnswer)
+        let arrayPossibleCharacters = Array(possibleCharactersString)
+        let setAnswer = Set(arrayAnswer)
+        let setPossibleCharacters = Set(arrayPossibleCharacters)
+        let commonCharacters = setAnswer.intersection(setPossibleCharacters).count
+        if unwrappedAnswer.count == 4 && commonCharacters == 4
         {
             tries+=1
-            //TODO: check if answer is correct and return true or false. First must have generated an answer
+            let result = CompareStrings(unwrappedAnswer, correctAnswer)
+            if result.exactMatches == 4
+            {
+                return true
+            }
+            else
+            {
+                print("Exact matches: \(result.exactMatches)")
+                print("Non-exact matches: \(result.nonExactMatches)")
+                return false
+            }
         }
         else
         {
@@ -54,3 +70,43 @@ func AwaitAnswer() -> Bool
     }
     return false
 }
+
+func CompareStrings(_ str1: String, _ str2: String) -> (exactMatches: Int, nonExactMatches: Int)
+{
+    var exactMatches = 0
+    var nonExactMatches = 0
+    // Convert strings to arrays for easier indexing
+    let arr1 = Array(str1)
+    let arr2 = Array(str2)
+
+    // Check for exact matches
+    for i in 0..<4 {
+        if arr1[i] == arr2[i] {
+            exactMatches += 1
+        }
+    }
+
+    // Use sets to find common characters
+    let set1 = Set(arr1)
+    let set2 = Set(arr2)
+    let commonCharacters = set1.intersection(set2)
+    for char in commonCharacters {
+        var inStr1ButNotExact = false
+        var inStr2ButNotExact = false
+        
+        for i in 0..<4 {
+            if arr1[i] == char && arr2[i] != char {
+                inStr1ButNotExact = true
+            }
+            if arr2[i] == char && arr1[i] != char {
+                inStr2ButNotExact = true
+            }
+        }
+        
+        if inStr1ButNotExact && inStr2ButNotExact {
+            nonExactMatches += 1
+        }
+    }
+    
+    return (exactMatches, nonExactMatches)
+ }
